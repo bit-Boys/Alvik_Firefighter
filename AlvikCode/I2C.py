8b33e7c534fcdfe01a866530820c461304397937
bit-Boys <88783371+bit-Boys@users.noreply.github.com>
2024-11-17 13:14:24 -0500

    Update I2C.py

--- AlvikCode/I2C.py
+++ AlvikCode/I2C.py
@@ -6,20 +6,60 @@
 from arduino_alvik import ArduinoAlvik
 from time import sleep_ms
 import sys
-from machine import I2C
+import machine
+from machine import Pin, I2C
 
+A4 = 11                                         # ESP32 pin11 SDA -> nano A4
+A5 = 12                                         # ESP32 pin12 SCL -> nano A5
+ESP32_SDA = Pin(A4, Pin.OUT)                    # ESP32_SDA
+ESP32_SCL = Pin(A5, Pin.OUT)                    # ESP32_SCL
 
-i2c = I2C(0, freq=400000)          # create I2C at 400kHz
-
+print("works")
+          
 alvik = ArduinoAlvik()
-alvik.begin()
+#alvik.begin()
 
 
-address = i2c.scan()[0]     # usually 43
-sleep_ms(500)
+i2c = machine.SoftI2C(ESP32_SCL, ESP32_SDA, freq=50000)   # create I2C at 100kHz
+i2c.init(ESP32_SCL, ESP32_SDA, freq=50000)
 
-command = b''
-data = b''
+
+scans = []
+while True:
+  scans = i2c.scan()
+  print(scans)
+  sleep_ms(25)
+
+  if scans != []:
+    print("WORKED GOT")
+    print(scans[0])
+    try:
+      for i in range(len(scans)):
+        val = i2c.readfrom_mem(scans[i], 0x08 , 1) 
+        print(val)
+    except Exception as e:
+      print("Errored")
+      print(e)
+
+
+address = scans[0]
+
+print("Scanning...")
+
+count = 0
+command = b""
+
+
+# read
+while True:
+
+  command = i2c.readfrom(address, 1)  # read in 4 bytes
+  print(command)
+  sleep_ms(100)
+      
+  
+
+
 while True:
 
   data = i2c.readfrom(address, 2)  # read in 2 bytes MUST BE SENT IN 2 BYTE CHUNKS
