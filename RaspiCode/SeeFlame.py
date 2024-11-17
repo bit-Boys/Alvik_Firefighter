730c14ed675f2b8da4f05363fb5e5272ae99b9ff
bit-Boys <benjaminwellborn@gmail.com>
2024-11-16 20:18:18 -0500

    Wrote connection code for raspi

--- RaspiCode/SeeFlame.py
+++ RaspiCode/SeeFlame.py
@@ -1,9 +1,17 @@
 # Navigates through the maze, detecting candles and going toward them
 
+import smbus
+import time
 
 
+bus = smbus.SMBus(0)
+address = 0x60   # May change
+command_address = 0x00   # Keep here unless we need to send a lot of data, like "page" of a book
 
 
+data = b'F2.'
+
+bus.write_byte_data(address, command_address, data)
 
 
 
